11d86d4e115866a5e22acf38c7af837fdc5ecf4c
muthukoornandini <muthukoornandini@gmail.com>
2026-08-07 18:19:28 +0530

    Fix backend API URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://invoice-management-final.onrender.com/api",
+  baseURL: "https://invoice-management-system-api-z5lx.onrender.com/api",
 });
 
 export default API;
