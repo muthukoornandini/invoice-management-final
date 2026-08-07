0df8df31478bf87f56b83a74068c0882ae22bd70
muthukoornandini <muthukoornandini@gmail.com>
2026-08-07 06:50:25 +0530

    Create database tables

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { connectDB } = require("./config/db");
+const { connectDB, sequelize } = require("./config/db");
+
+const User = require("./models/User");
 
 const authRoutes = require("./routes/authRoutes");
 const customerRoutes = require("./routes/customerRoutes");
@@ -12,15 +14,21 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
 // Database Connection
 connectDB();
 
+// Create database tables
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log("Database Synced");
+  })
+  .catch((err) => {
+    console.error("Sync Error:", err);
+  });
 
 // API Routes
 app.use("/api/auth", authRoutes);
@@ -29,16 +37,13 @@ app.use("/api/products", productRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-
 // Test Route
 app.get("/", (req, res) => {
   res.send("Invoice Management Backend Running");
 });
 
-
 // Server Port
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
